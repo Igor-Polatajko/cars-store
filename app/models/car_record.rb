579d4952e6b23f9922a11fb2232b2ad912523316
Igor Polatajko <igor.bogdanovich39@gmail.com>
2020-04-10 01:35:32 +0300

    Added carousel

--- app/models/car_record.rb
+++ app/models/car_record.rb
@@ -2,4 +2,5 @@ class CarRecord < ApplicationRecord
     has_many_attached :images
     validates :title, :description, :price, :images, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :images, length: {maximum:5, message: "maximum number of images is 5"}
 end
