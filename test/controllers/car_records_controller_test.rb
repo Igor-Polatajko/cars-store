10f23d6888fccfdfb6ef27e56f3891dd2d1ccfb0
Igor Polatajko <igor.bogdanovich39@gmail.com>
2020-04-11 23:41:04 +0300

    Fixed controller test

--- test/controllers/car_records_controller_test.rb
+++ test/controllers/car_records_controller_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class CarRecordsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    images = [fixture_file_upload('./files/test-image.jpg', 'image')]
+    @images = [fixture_file_upload('./files/test-image.jpg', 'image')]
     @car_record_one = car_records(:one)
     @car_record_two = car_records(:two)
-    @car_record_one.images = images
-    @car_record_two.images = images
+    @car_record_one.images = @images
+    @car_record_two.images = @images
 
     @car_record_one.save
     @car_record_two.save
@@ -22,20 +22,19 @@ class CarRecordsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # ToDo Update it later
-  # test "should create car_record" do
-  #   assert_difference(['CarRecord.count', 'ActiveStorage::Attachment.count']) do
-  #     post car_records_url, params: { car_record: { 
-  #       description: @car_record_one.description,
-  #       price: @car_record_one.price, 
-  #       title: @car_record_one.title, 
-  #       images: @car_record_one.images
-  #      } 
-  #    }
-  #   end
+  test "should create car_record" do
+    assert_difference(['CarRecord.count', 'ActiveStorage::Attachment.count']) do
+      post car_records_url, params: { car_record: { 
+        description: @car_record_one.description,
+        price: @car_record_one.price, 
+        title: @car_record_one.title, 
+        images: @images
+       } 
+     }
+    end
 
-  #   assert_redirected_to car_record_url(CarRecord.last)
-  # end
+    assert_redirected_to car_record_url(CarRecord.last)
+  end
 
   test "should show car_record" do
     get car_record_url(@car_record_one)
@@ -55,7 +54,7 @@ class CarRecordsControllerTest < ActionDispatch::IntegrationTest
          description: @car_record_one.description,
          price: @car_record_one.price, 
          title: @car_record_one.title, 
-         images: @car_record_one.images
+         images: @images
         } 
       }
     assert_redirected_to car_record_url(@car_record_one)
