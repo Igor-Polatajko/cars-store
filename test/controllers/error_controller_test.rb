99e2169db7e1cf829f6a9b32fd65dc7b1157b9a9
Igor Polatajko <igor.bogdanovich39@gmail.com>
2020-04-18 20:15:48 +0300

    tests updated

--- test/controllers/error_controller_test.rb
+++ test/controllers/error_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ErrorControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get error_show_url
+    get error_path
     assert_response :success
   end
 
