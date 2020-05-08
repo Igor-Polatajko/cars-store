3a9bf91a27bd770082a8cc1c12146ef2fd68b611
Igor Polatajko <igor.bogdanovich39@gmail.com>
2020-05-09 00:37:37 +0300

    Added after order submit view

--- app/controllers/order_request_controller.rb
+++ app/controllers/order_request_controller.rb
@@ -4,5 +4,20 @@ class OrderRequestController < ApplicationController
   end
 
   def create
+    if !request.post?
+      redirect_to main_page_index_path
+      return
+    end
+
+    #send email with confirmation link
+
+    render :template => "order_request/submitted"
   end
+
+  def confirm
+    #activate the order
+
+    #send email to car owner
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   delete 'user/destroy'
   
   get 'order_requests/new/:id', to: 'order_request#new', as: 'new_order_request'
-  post  '/order_requests', to: 'order_request#create'
+  match  '/order_requests', to: 'order_request#create', via: [:get, :post]
 
   get 'error/show', as: 'error'
   root 'main_page#index', as: 'main_page_index'
