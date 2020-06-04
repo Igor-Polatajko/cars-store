53f88aba6ebff69179ae35fb927ee834e403c20f
Igor Polatajko <igor.bogdanovich39@gmail.com>
2020-06-05 00:11:52 +0300

    Fixed links

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,6 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/send_confirmation_token
   def send_confirmation_token
     UserMailer.send_confirmation_token
   end
